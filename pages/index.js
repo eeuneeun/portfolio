a336dce8aa4b33dd27ec48c22688575695ba5ef7
wej <wej@openkps.com>
2023-04-05 13:36:01 +0900

    컨텐츠 추가

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
+import Link from "next/link.js";
 import React, { useState, useEffect } from "react";
 import Footer from "../components/layout/Footer.js";
+import { prefix } from "../config/config.js";
 
 export default function Home() {
   return (
@@ -57,24 +59,167 @@ export default function Home() {
           <div className="inner">
             <h2>PORTFOLIO</h2>
             <article>
-              <ul>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
+              <ul className="portfolio-list flex-start">
+                <li>
+                  <Link
+                    href="http://www.khma.org/portal/portal.web"
+                    target="blank"
+                    passHref
+                    legacyBehavior
+                  >
+                    <a>
+                      <img
+                        src={`${prefix}/img/web01.png`}
+                        alt="대한주택관리사협회"
+                      />
+                    </a>
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="https://www.coinbit.com/main.do"
+                    target="blank"
+                    passHref
+                    legacyBehavior
+                  >
+                    <a>
+                      <img
+                        src={`${prefix}/img/web02.png`}
+                        alt="코인빗 랜딩 페이지"
+                      />
+                    </a>
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="https://www.coinbit.com/order.do?code=BTC-ETH"
+                    target="blank"
+                    passHref
+                    legacyBehavior
+                  >
+                    <a>
+                      <img
+                        src={`${prefix}/img/web03.png`}
+                        alt="코인빗 거래소"
+                      />
+                    </a>
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="https://www.lotteon.com/p/display/main/lottemart?mall_no=4"
+                    target="blank"
+                    passHref
+                    legacyBehavior
+                  >
+                    <a>
+                      <img src={`${prefix}/img/web04.png`} alt="롯데마트" />
+                    </a>
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="https://hanacard.bizcc.co.kr:61512/"
+                    target="blank"
+                    passHref
+                    legacyBehavior
+                  >
+                    <a>
+                      <img
+                        src={`${prefix}/img/web05.png`}
+                        alt="하나카드 현대해상 파이낸스 세이프보험"
+                      />
+                    </a>
+                  </Link>
+                </li>
+
+                <li>
+                  <Link
+                    href="https://hanacard.bizcc.co.kr:61512/call"
+                    target="blank"
+                    passHref
+                    legacyBehavior
+                  >
+                    <a>
+                      <img
+                        src={`${prefix}/img/web06.png`}
+                        alt="하나카드 현대해상 파이낸스 세이프보험 상담원용 관리자 페이지"
+                      />
+                    </a>
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="https://chatbot.lotterentacar.net/chat/P2tpY6DXUmRNHKPix/1874b5274aeTlWK"
+                    target="blank"
+                    passHref
+                    legacyBehavior
+                  >
+                    <a>
+                      <img
+                        src={`${prefix}/img/web07.png`}
+                        alt="롯데렌터카 챗봇"
+                      />
+                    </a>
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="https://www.gogung.go.kr/gogung/main/main.do"
+                    target="blank"
+                    passHref
+                    legacyBehavior
+                  >
+                    <a>
+                      <img src={`${prefix}/img/web08.png`} alt="고궁박물관" />
+                    </a>
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="https://dgovkorea.go.kr/"
+                    target="blank"
+                    passHref
+                    legacyBehavior
+                  >
+                    <a>
+                      <img
+                        src={`${prefix}/img/web09.png`}
+                        alt="디지털 전자정부 홍보관"
+                      />
+                    </a>
+                  </Link>
+                </li>
               </ul>
             </article>
           </div>
         </div>
         <div id="sns" className="sns">
-          <div className="inner">
+          <div className="inner flex-col-center">
             <h2>SNS</h2>
             <article>
-              <ul>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
+              <ul className="sns-list flex-center">
+                <li>
+                  <Link href="">kakaotalk</Link>
+                </li>
+                <li>
+                  <Link href="https://blog.naver.com/sacroo" target="_blank">
+                    naver blog
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="https://www.instagram.com/sacroosy/"
+                    target="_blank"
+                  >
+                    instagram
+                  </Link>
+                </li>
+                <li>
+                  <Link href="" target="_blank">
+                    twitter
+                  </Link>
+                </li>
               </ul>
             </article>
           </div>
