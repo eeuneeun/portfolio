37443b5bdbfd749b8229eeb6642d0fe6e0212f86
wej <wej@openkps.com>
2023-04-24 01:26:47 +0900

    Updates

--- _next/static/n5DMBLhsf9SSpUGD6YWS7/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/66-711008562e66a906.js","static/chunks/pages/index-ad46bea7fcda08b4.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
