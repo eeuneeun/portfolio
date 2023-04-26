04aa00a8f6f5c18781efc0df425ff8553ead0f23
wej <wej@openkps.com>
2023-04-26 17:16:09 +0900

    모바일 메뉴 구현

--- components/layout/Gnb.js
+++ components/layout/Gnb.js
@@ -11,6 +11,18 @@ import Link from "next/link";
 const domainUrl = process.env.NEXT_PUBLIC_DOMAIN_URL;
 
 export default function Gnb(props) {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  function toggleMobileMenu(action) {
+    if (action == "open") {
+      setIsMobileMenuOpen(true);
+    } else if (action == "close") {
+      setIsMobileMenuOpen(false);
+    } else {
+      setIsMobileMenuOpen(false);
+    }
+  }
+
   // * useEffect 선언부 *
   useEffect(() => {}, []);
 
@@ -40,26 +52,46 @@ export default function Gnb(props) {
           </ul>
         </div>
         <div className="inner flex-between mobile">
-          <h1 className="title">
-            <Link href="/">EUN</Link>
-          </h1>
-          <div className="ham-menu">
-            <button className="close">닫기</button>
+          <div className="title-wrap flex-between">
+            <h1 className="title">
+              <Link href="/">EUN</Link>
+            </h1>
+            <button
+              className="ham-menu-btn"
+              onClick={() => toggleMobileMenu("open")}
+            >
+              닫기
+            </button>
+          </div>
+          <div className={`${isMobileMenuOpen ? "active" : ""} ham-menu`}>
+            <button className="close" onClick={() => toggleMobileMenu("close")}>
+              닫기
+            </button>
             <ul>
               <li>
-                <Link href={`${prefix}/`}>HOME</Link>
+                <Link href={`${prefix}/`} passHref legacyBehavior>
+                  <a onClick={() => toggleMobileMenu("close")}>HOME</a>
+                </Link>
               </li>
               <li>
-                <Link href={`${prefix}/#profile`}>PROFILE</Link>
+                <Link href={`${prefix}/#profile`} passHref legacyBehavior>
+                  <a onClick={() => toggleMobileMenu("close")}>PROFILE</a>
+                </Link>
               </li>
               <li>
-                <Link href={`${prefix}/#portfolio`}>PORTFOLIO</Link>
+                <Link href={`${prefix}/#portfolio`} passHref legacyBehavior>
+                  <a onClick={() => toggleMobileMenu("close")}>PORTFOLIO</a>
+                </Link>
               </li>
               <li>
-                <Link href={`${prefix}/#sns`}>SNS</Link>
+                <Link href={`${prefix}/#sns`} passHref legacyBehavior>
+                  <a onClick={() => toggleMobileMenu("close")}>SNS</a>
+                </Link>
               </li>
               <li>
-                <Link href={`${prefix}/#contact`}>CONTACT</Link>
+                <Link href={`${prefix}/#contact`} passHref legacyBehavior>
+                  <a onClick={() => toggleMobileMenu("close")}>CONTACT</a>
+                </Link>
               </li>
             </ul>
           </div>
