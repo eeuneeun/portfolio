9f54e86f19bb5c0b14ca7e0b99cf8931683da342
eeuneeun <sacroo89@gmail.com>
2023-09-20 13:46:45 +0900

    Updates

--- _next/static/QfbC5T_RymU1jH7vqFcD2/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/66-711008562e66a906.js","static/chunks/pages/index-12af8abf63613b78.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
