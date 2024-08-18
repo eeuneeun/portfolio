886df506ab57d234050017aea14ec901288d96bb
euneun <sacroo@naver.com>
2024-08-18 09:53:50 +0900

    Draft : 임시로 다운로드나 새창 열림으로 문서 처리, 뷰어 추후 작업 할 것

--- components/pieces/MainDocument.js
+++ components/pieces/MainDocument.js
@@ -9,32 +9,28 @@ export default function JjabixDocument() {
         <h5>Jjabix MainDocument</h5>
         <ul className="flex-center">
           <li>
-            <Link
-              href={`${prefix}/doc/jjabix_ach.pdf`}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img src={`${prefix}/img/idea_icon.png`} alt="idea" />
+            <Link href={`${prefix}/doc/jjabix_ach.pdf`} passHref>
+              <a target="_blank" rel="noopener noreferrer">
+                <img src={`${prefix}/img/idea_icon.png`} alt="idea" />
+              </a>
             </Link>
           </li>
           <li>
-            <Link
-              href={`${prefix}/doc/jjabix_ppt.pdf`}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img src={`${prefix}/img/ppt_icon.png`} alt="ppt" />
+            <Link href={`${prefix}/doc/jjabix_ppt.pdf`} passHref>
+              <a target="_blank" rel="noopener noreferrer">
+                <img src={`${prefix}/img/ppt_icon.png`} alt="ppt" />
+              </a>
             </Link>
           </li>
           <li>
             <Link
               onClick={() => alert("ppt는 다운로드 됩니다!")}
               href={`${prefix}/doc/jjabix_ppt.pptx`}
-              target="_blank"
-              rel="noopener noreferrer"
-              download
+              passHref
             >
-              <img src={`${prefix}/img/pdf_icon.png`} alt="pdf" />
+              <a target="_blank" rel="noopener noreferrer" downloads>
+                <img src={`${prefix}/img/pdf_icon.png`} alt="pdf" />
+              </a>
             </Link>
           </li>
         </ul>
