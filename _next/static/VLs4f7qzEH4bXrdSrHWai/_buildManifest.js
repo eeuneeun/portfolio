353f5e53bb3daedd2b5d6a0489d9a0d5c09d8d17
euneun <sacroo@naver.com>
2024-08-18 09:36:38 +0900

    Updates

---
+++ _next/static/VLs4f7qzEH4bXrdSrHWai/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/540-98ef4253f81410d8.js","static/chunks/pages/index-e9499014c862097e.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
